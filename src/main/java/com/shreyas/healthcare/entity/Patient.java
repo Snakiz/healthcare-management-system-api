01d484537d2892a2b1eceb500dc1e7a9490999ad
snaki_z <shreyashlzanjal@gmail.com>
2026-05-15 11:00:49 +0530

    feat: Added Patient Entity, PatientRepository, PatientRequestDto, PatientResponseDto and PatientService

---
+++ src/main/java/com/shreyas/healthcare/entity/Patient.java
@@ -0,0 +1,89 @@
+package com.shreyas.healthcare.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "patients")
+public class Patient extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "age", nullable = false)
+    private Integer age;
+
+    @Column(name = "gender", nullable = false)
+    private String gender;
+
+    public Patient() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ src/main/java/com/shreyas/healthcare/service/PatientService.java
@@ -0,0 +1,12 @@
+package com.shreyas.healthcare.service;
+
+import com.shreyas.healthcare.dto.request.PatientRequestDto;
+import com.shreyas.healthcare.dto.response.PatientResponseDto;
+
+public class PatientService {
+
+    PatientResponseDto registerPatient(PatientRequestDto patientRequestDto) {
+        return new PatientResponseDto();
+    }
+}
+
